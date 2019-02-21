4a905a89826eb1c3472a1e7a75b39850fefe60d9
Will <megapocketninja@gmail.com>
2019-02-21 12:18:39 -0500

    Building user model

--- checkn_app/models.py
+++ checkn_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    auth_id = models.CharField(max_length=100)
+    lon = models.DecimalField(max_digits=9, decimal_places=6)
+    lat = models.DecimalField(max_digits=9, decimal_places=6)
+    friends = models.ForeignKey(User, on_delete=models.CASCADE, related_name="friends")
+
+    def __str__(self):
+        return self.name
