8ef7c4cb02ef50b31793bbeabf0b050bf497b660
Will <megapocketninja@gmail.com>
2019-02-21 16:00:54 -0500

    Added place model

--- checkn_app/admin.py
+++ checkn_app/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
-from .models import User
+from .models import User, Place
 
 
 # Register your models here.
-admin.site.register([User])
+admin.site.register([User, Place])
--- checkn_app/models.py
+++ checkn_app/models.py
@@ -1,15 +1,31 @@
 from django.db import models
 
 # Create your models here.
+class Place(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
 class User(models.Model):
     name = models.CharField(max_length=100)
     auth_id = models.CharField(max_length=100)
     lon = models.DecimalField(max_digits=9, decimal_places=6, default=0.0)
     lat = models.DecimalField(max_digits=9, decimal_places=6, default=0.0)
     friends = models.ManyToManyField(
-        "self", default=1, related_name="friends_list", blank=True, null=True
+        "self", default=1, related_name="friends_list", blank=True
     )
     status = models.CharField(max_length=100)
+    place = models.ForeignKey(
+        Place,
+        default=1,
+        on_delete=models.CASCADE,
+        related_name="place_name",
+        blank=True,
+        null=True,
+    )
 
     def __str__(self):
         return self.name
+
