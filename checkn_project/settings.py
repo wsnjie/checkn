e060a7d1c67856586336b7079cf5dac47544fa1a
Will <megapocketninja@gmail.com>
2019-02-28 10:54:01 -0500

    Added views and urls

--- checkn_project/settings.py
+++ checkn_project/settings.py
@@ -16,6 +16,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PROJECT_PATH = os.path.realpath(os.path.dirname(__file__))
+REACT_APP_DIR = os.path.join(BASE_DIR, "client")
 
 
 # Quick-start development settings - unsuitable for production
