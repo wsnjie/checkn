6aa6a37c1a96891175e66306578d1b5438ef440c
Will <megapocketninja@gmail.com>
2019-02-28 10:18:00 -0500

    update settings

--- checkn_project/settings.py
+++ checkn_project/settings.py
@@ -12,9 +12,6 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
-import django_heroku
-  django_heroku.settings(locals())
-
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -182,3 +179,6 @@ CSRF_COOKIE_NAME = "XSRF-TOKEN"
 SITE_ID = 1
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
+import django_heroku
+
+django_heroku.settings(locals())
